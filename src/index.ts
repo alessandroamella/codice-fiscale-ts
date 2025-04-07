d9d5b68657fd03d56336d9b227aa1d9197b1d063
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2025-04-07 12:44:18 +0200

    fix: ficx typing of getMunicipalities and getCountries

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,50 @@
 export {
-  calculateCheckCharacter,
-  calculateDayGenderCode,
-  calculateFirstNameCode,
-  calculateFiscalCode,
-  calculateLastNameCode,
-  calculateMonthCode,
-  calculateYearCode,
-  decodeDay,
-  decodeFiscalCode,
-  decodeMonth,
-  decodeYear,
-  extractConsonants,
-  extractVowels,
-  findBirthPlaceByCode,
-  getCountryCode,
-  getMunicipalCodeFromPlace,
-  isValidFiscalCode,
-  normalizeString
+	calculateCheckCharacter,
+	calculateDayGenderCode,
+	calculateFirstNameCode,
+	calculateFiscalCode,
+	calculateLastNameCode,
+	calculateMonthCode,
+	calculateYearCode,
+	decodeDay,
+	decodeFiscalCode,
+	decodeMonth,
+	decodeYear,
+	extractConsonants,
+	extractVowels,
+	findBirthPlaceByCode,
+	getCountryCode,
+	getMunicipalCodeFromPlace,
+	isValidFiscalCode,
+	normalizeString
 } from './codiceFiscale.ts';
 
 export type {
-  BasePerson,
-  BilingualMunicipality,
-  Country,
-  FiscalCodeData,
-  ForeignPerson,
-  ItalianMunicipality,
-  ItalianPerson,
-  Municipality,
-  Person
+	BasePerson,
+	BilingualMunicipality,
+	Country,
+	FiscalCodeData,
+	ForeignPerson,
+	ItalianMunicipality,
+	ItalianPerson,
+	Municipality,
+	Person
 } from './types.ts';
 
 export {
-  isForeignPerson,
-  isItalianPerson,
-  validatePerson
+	isForeignPerson,
+	isItalianPerson,
+	validatePerson
 } from './types.ts';
 
-export async function getMunicipalities() {
-  const { municipalities } = await import('./data/municipalities.ts');
-  return municipalities;
+import type { Country, Municipality } from './types.ts';
+
+export async function getMunicipalities(): Promise<Municipality[]> {
+	const { municipalities } = await import('./data/municipalities.ts');
+	return municipalities;
 }
 
-export async function getCountries() {
-  const { countries } = await import('./data/countries.ts');
-  return countries;
+export async function getCountries(): Promise<Country[]> {
+	const { countries } = await import('./data/countries.ts');
+	return countries;
 }
